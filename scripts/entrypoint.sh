21ea8a1f022ab7a530d28c28d8822e9bd519e6c7
AtheeshRathnaweera <rathnaweeraatheesh72@gmail.com>
2025-11-09 01:57:23 +0530

    refactor: enhance entrypoint.sh with clearer migration and database wait messages

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -11,10 +11,17 @@ if [[ "$PRINT_ENV_ON_LOAD" = true || "$PRINT_ENV_ON_LOAD" = True ]]; then
 fi
 
 if [[ "$WAIT_FOR_DB" = true || "$WAIT_FOR_DB" = True ]]; then
+  echo ">>> Waiting for database..."
   dockerize \
     -wait tcp://$DB_HOST:$DB_PORT \
     -timeout 300s
 fi
+
+############################################################################
+# Run Alembic migrations
+############################################################################
+echo ">>> Running Alembic migrations..."
+alembic upgrade head || { echo "Alembic migration failed"; exit 1; }
 
 ############################################################################
 # Start App
