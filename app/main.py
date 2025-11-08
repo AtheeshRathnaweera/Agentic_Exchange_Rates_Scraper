221840c22a001a713ba1c86addacbd66cd45a809
AtheeshRathnaweera <rathnaweeraatheesh72@gmail.com>
2025-11-09 01:56:18 +0530

    refactor: update imports and configuration paths in main.py for clarity

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from fastapi import Request
 
 from agno.os import AgentOS
@@ -6,15 +5,15 @@ from agno.run.workflow import (
     WorkflowRunOutput,
 )
 from agno.utils.pprint import pprint_run_response
-from agents.core_agent import get_core_agent
 
+from agents import get_core_agent
 from workflows import get_scrape_rates_workflow
 
-OS_CONFIG_PATH = str(Path(__file__).parent.joinpath("config.yaml"))
+OS_CONFIG_PATH = "configs/agent_os_config.yaml"
 
 # web_agent = get_web_agent(model_id="gpt-5")
 # agno_assist = get_agno_assist(model_id="gpt-5")
-core_agent = get_core_agent(model_id="openai/gpt-oss-120b", debug_mode=True)
+core_agent = get_core_agent(debug_mode=True)
 # scrape_rates_workflow = scrape_rates.workflow.get_scrape_rates_workflow()
 
 # Create the AgentOS
@@ -33,9 +32,7 @@ async def run_scraper(request: Request):
     body = await request.body()
     print(f"Main: run scraper started: {body.decode('utf-8')}")
     try:
-        response: WorkflowRunOutput = await get_scrape_rates_workflow().arun(
-            input="Test Input",
-        )
+        response: WorkflowRunOutput = await get_scrape_rates_workflow().arun()
         pprint_run_response(response, markdown=True)
     except Exception as e:
         print(f"❌ Error: {e}")
