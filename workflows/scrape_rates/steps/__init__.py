ff631373e681fd6a936f8bc0b4eef959a6f2e5f7
AtheeshRathnaweera <rathnaweeraatheesh72@gmail.com>
2025-11-10 09:01:56 +0530

    style: format __all__ declaration for improved readability

--- workflows/scrape_rates/steps/__init__.py
+++ workflows/scrape_rates/steps/__init__.py
@@ -2,4 +2,8 @@ from .get_urls_step import get_urls_step
 from .extract_rates_step import extract_rates_step
 from .save_to_db_step import save_to_db_step
 
-__all__ = ["get_urls_step", "extract_rates_step", "save_to_db_step"]
+__all__ = [
+    "get_urls_step",
+    "extract_rates_step",
+    "save_to_db_step",
+]
