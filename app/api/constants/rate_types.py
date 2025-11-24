69d65cbd7a5658a58f5f04245dff47ba670fe326
AtheeshRathnaweera <rathnaweeraatheesh72@gmail.com>
2025-11-24 14:07:25 +0530

    feat: add rate types constants for currency exchange operations

---
+++ app/api/constants/rate_types.py
@@ -0,0 +1,12 @@
+RATE_TYPES = {
+    "tt_buying": "TT Buying",
+    "tt_selling": "TT Selling",
+    "draft_buying": "Draft Buying",
+    "draft_selling": "Draft Selling",
+    "cheques_buying": "Cheques Buying",
+    "cheques_selling": "Cheques Selling",
+    "currency_buying": "Currency Buying",
+    "currency_selling": "Currency Selling",
+    "other_buying": "Other Buying",
+    "other_selling": "Other Selling",
+}
