7890e021b4f07d1e94302198cb9e56463f9c26b2
AtheeshRathnaweera <rathnaweeraatheesh72@gmail.com>
2025-11-25 22:13:22 +0530

    refactor: simplify rate types by consolidating buying and selling options

--- app/api/constants/rate_types.py
+++ app/api/constants/rate_types.py
@@ -1,12 +1,7 @@
 RATE_TYPES = {
-    "tt_buying": "TT Buying",
-    "tt_selling": "TT Selling",
-    "draft_buying": "Draft Buying",
-    "draft_selling": "Draft Selling",
-    "cheques_buying": "Cheques Buying",
-    "cheques_selling": "Cheques Selling",
-    "currency_buying": "Currency Buying",
-    "currency_selling": "Currency Selling",
-    "other_buying": "Other Buying",
-    "other_selling": "Other Selling",
+    "tt": "Telegraphic Transfers",
+    "draft": "Draft",
+    "cheques": "Cheques",
+    "currency": "Currency",
+    "other": "Other",
 }
